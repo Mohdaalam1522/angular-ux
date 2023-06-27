5a4870d99f8a98164281be412bcfef2d425581e7
Mohd Aalam <mohdaalam1522@gmail.com>
2023-06-27 19:19:05 +0530

    Data Transfer from Parent To Child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,36 @@ import { Customer } from './my-child/my-child.component';
 })
 export class AppComponent {
   title = 'mytestAalam';
-  name: string= 'Aalam';
+  myName: string= 'Aalam from parent';
+  childName:string="Mohd Aalam";
   color: any
   dataFromParent: Customer[] | undefined;
-
+  myToggleFlagFromChild: boolean | undefined;
+  myToggleflagdata:string | undefined;
+  myToggleflag:string | undefined;
+  myFlagData: string | undefined;
   myChildData(msg: Customer[]){
     console.log(msg);
     this.dataFromParent = msg;
   }
+
+  toggleData(toggleFlag: boolean){
+this.myToggleFlagFromChild = toggleFlag
+  }
+  data(toggleData:string){
+    this.myToggleflagdata= toggleData 
+  }
+  text(toggleData:string){
+    this.myToggleflag= toggleData
+   }
+   class(toggleData:string){
+     this.myFlagData=toggleData
+    }
+    parentData(toggleData:string){
+      this.myName=toggleData
+    }
+    
+   
+   
 }
 
